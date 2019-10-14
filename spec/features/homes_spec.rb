f695a33b66212699e2af229a29feb07dc08eb2b5
NaoNao15 <basketball.nao15@icloud.com>
2019-10-14 19:30:50 +0900

    やり直し

--- spec/features/homes_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-RSpec.feature "Homes", type: :feature do
-  scenario "アクセスした際にリンクがある" do
-    visit root_path
-    expect(page).to have_link "投稿する"
-    expect(page).to have_link "新規投稿"
-  end
-end
