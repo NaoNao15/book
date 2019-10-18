1ca07da3dd78477fa26015f0b1e23143a29cea0b
NaoNao15 <basketball.nao15@icloud.com>
2019-10-18 17:59:22 +0900

    cpmtrollers/posts_controller_spec.rbテスト実装（new,create,destroy）

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,4 +1,130 @@
 require 'rails_helper'
 
 RSpec.describe PostsController, type: :controller do
+  describe "newページ表示" do
+    let!(:user) { create(:user) }
+    let!(:post) { create(:post, user: user) }
+
+    context "ログイン済みのユーザー" do
+      before do
+        sign_in user
+        get :new
+      end
+
+      it "正常にレスポンスを返す" do
+        expect(response).to be_successful
+      end
+
+      it "200レスポンスを返す" do
+        expect(response).to have_http_status "200"
+      end
+
+      it "indexテンプレートを表示させる" do
+        expect(response).to render_template :new
+      end
+
+      it "@postが期待される値をもつ" do
+        expect(assigns(:post)).not_to match_array post
+      end
+    end
+
+    context "ログインしていないユーザー" do
+      before do
+        get :new
+      end
+
+      it "レスポンスに失敗する" do
+        expect(response).not_to be_successful
+      end
+
+      it "200レスポンスを返さない" do
+        expect(response).not_to have_http_status "200"
+      end
+
+      it "indexテンプレートを表示できない" do
+        expect(response).not_to render_template :new
+      end
+
+      it "@postが期待される値をもつ" do
+        expect(assigns(:post)).not_to match_array post
+      end
+    end
+  end
+
+  describe "POST #create" do
+    let!(:user) { create(:user) }
+    let(:post_attributes) { attributes_for(:post) }
+
+    context "ログイン済みのユーザー" do
+      before do
+        sign_in user
+      end
+
+      it "302レスポンスを返す" do
+        post :create, params: { post: post_attributes }
+        expect(response).to have_http_status "302"
+      end
+
+      it "投稿成功後、トップページへ" do
+        post :create, params: { post: post_attributes }
+        expect(response).to redirect_to root_path
+      end
+
+      it 'userを削除すると、userが投稿したpostも削除される' do
+        user.posts.create( content: 'あとで書く')
+        expect{ user.destroy }.to change{ Post.count }.by(-1)
+      end
+    end
+
+    context "ログインしていないユーザー" do
+      it "レスポンスが失敗する" do
+        post :create, params: { post: post_attributes }
+        expect(response).not_to be_successful
+      end
+
+      it "ログインしていないユーザーは投稿できない" do
+        post :create, params: { post: post_attributes }
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
+  end
+
+  describe "DELETE #destroy" do
+    let!(:user) { create(:user) }
+    let!(:post) { create(:post, user: user) }
+
+    context "ログイン済みのユーザー" do
+      before do
+        sign_in user
+      end
+
+      it '投稿を消去する' do
+        expect do
+          delete :destroy, params: { id: post.id }
+        end.to change(Post, :count).by(-1)
+      end
+
+      it "302レスポンスを返す" do
+        delete :destroy, params: { id: post.id }
+        expect(response).to have_http_status "302"
+      end
+
+      it "投稿削除成功後、トップページかユーザー個別ページへ" do
+        delete :destroy, params: { id: post.id }
+        expect(response).to redirect_to root_path || users_path(user)
+      end
+    end
+
+    context "ログインしていないユーザー" do
+      it "レスポンスが失敗する" do
+        delete :destroy, params: { id: post.id }
+        expect(response).not_to be_successful
+      end
+
+      it "ログインしていないユーザーは投稿を削除できない" do
+        delete :destroy, params: { id: post.id }
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
+  end
 end
