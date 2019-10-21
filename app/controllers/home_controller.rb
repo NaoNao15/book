2e9fad5e5427ad4c11799c767d89371d142862a3
NaoNao15 <basketball.nao15@icloud.com>
2019-10-21 18:34:48 +0900

    検索機能実装

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,11 @@
 class HomeController < ApplicationController
   def index
-    @posts = Post.all.order(created_at: :desc).paginate(page: params[:page])
+    if params[:q]
+      @search = Post.ransack(params[:q])
+      @posts = @search.result.order(created_at: :desc).
+               paginate(page:params[:page])
+    else
+      @posts = Post.all.order(created_at: :desc).paginate(page: params[:page])
+    end
   end
 end
