336cf43cb5da6eeff0fd4d16e7bf9c31b6a3d427
NaoNao15 <basketball.nao15@icloud.com>
2019-10-22 10:13:35 +0900

    コメント機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root to: 'home#index'
-
   devise_for :users, controllers: {
     registrations: 'users/registrations',
   }
@@ -13,7 +12,7 @@ Rails.application.routes.draw do
   resources :posts, only: [:new, :show, :create, :destroy] do
     resources :likes, only: [:create, :destroy]
     resources :stockposts, only: [:create, :destroy]
+    resources :comments, only: [:create, :destroy]
   end
   resources :relationships, only: [:create, :destroy]
-  # resources :likes, only: [:create, :destroy]
 end
