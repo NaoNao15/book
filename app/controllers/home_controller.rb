6b2724aa685ca66e5e4ad71268234cb96b2bd44f
NaoNao15 <basketball.nao15@icloud.com>
2019-10-28 14:45:53 +0900

    stock-trends機能追加

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,10 +3,14 @@ class HomeController < ApplicationController
     if params[:q]
       @search = Post.ransack(params[:q])
       @posts = @search.result.order(created_at: :desc).paginate(page: params[:page], per_page: 10)
-      @trend_posts = @search.result.order(created_at: :desc).paginate(page: params[:page], per_page: 10)
+      @like_trend_posts = @search.result.joins("left join likes on posts.id = likes.post_id").group("posts.id").order(Arel.sql("count(likes.id) desc")).paginate(page: params[:page], per_page: 10)
+      @stock_trend_posts = @search.result.joins("left join stockposts on posts.id = stockposts.post_id").group("posts.id").order(Arel.sql("count(stockposts.id) desc")).paginate(page: params[:page], per_page: 10)
     else
       @posts = Post.all.paginate(page: params[:page], per_page: 10).order(created_at: :desc)
-      @trend_posts = Post.joins("left join likes on posts.id = likes.post_id").group("posts.id").order(Arel.sql("count(likes.id) desc"))
+      # Likeが多い順に入れ替えて代入
+      @like_trend_posts = Post.joins("left join likes on posts.id = likes.post_id").group("posts.id").order(Arel.sql("count(likes.id) desc"))
+      # Stockpostが多い順に入れ替えて代入
+      @stock_trend_posts = Post.joins("left join stockposts on posts.id = stockposts.post_id").group("posts.id").order(Arel.sql("count(stockposts.id) desc"))
     end
   end
 end
