ab917ce73592522b9ab55afe8d4e3bfd7093139a
NaoNao15 <basketball.nao15@icloud.com>
2019-10-28 15:11:12 +0900

    homeページのcssを変更、home/index.html.erbにページネーション追加

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,11 +6,11 @@ class HomeController < ApplicationController
       @like_trend_posts = @search.result.joins("left join likes on posts.id = likes.post_id").group("posts.id").order(Arel.sql("count(likes.id) desc")).paginate(page: params[:page], per_page: 10)
       @stock_trend_posts = @search.result.joins("left join stockposts on posts.id = stockposts.post_id").group("posts.id").order(Arel.sql("count(stockposts.id) desc")).paginate(page: params[:page], per_page: 10)
     else
-      @posts = Post.all.paginate(page: params[:page], per_page: 10).order(created_at: :desc)
+      @posts = Post.all.paginate(page: params[:page], per_page: 6).order(created_at: :desc)
       # Likeが多い順に入れ替えて代入
-      @like_trend_posts = Post.joins("left join likes on posts.id = likes.post_id").group("posts.id").order(Arel.sql("count(likes.id) desc"))
+      @like_trend_posts = Post.joins("left join likes on posts.id = likes.post_id").group("posts.id").order(Arel.sql("count(likes.id) desc")).paginate(page: params[:page], per_page: 6)
       # Stockpostが多い順に入れ替えて代入
-      @stock_trend_posts = Post.joins("left join stockposts on posts.id = stockposts.post_id").group("posts.id").order(Arel.sql("count(stockposts.id) desc"))
+      @stock_trend_posts = Post.joins("left join stockposts on posts.id = stockposts.post_id").group("posts.id").order(Arel.sql("count(stockposts.id) desc")).paginate(page: params[:page], per_page: 6)
     end
   end
 end
