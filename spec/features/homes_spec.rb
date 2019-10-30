63210f92973723eb7207f39f8db18e7faa5dcd86
NaoNao15 <basketball.nao15@icloud.com>
2019-10-30 19:55:35 +0900

    features/homes_spec.rbにテスト追加

--- spec/features/homes_spec.rb
+++ spec/features/homes_spec.rb
@@ -1,9 +1,26 @@
 require 'rails_helper'
 
 RSpec.feature "Homes", type: :feature do
+  given!(:user) { create(:user) }
+
   scenario "アクセスした際にリンクがある" do
     visit root_path
     expect(page).to have_link "Please post your book."
+  end
+
+  scenario "ログインしていないユーザーがAll usersをクリックするとユーザー一覧へ" do
+    visit root_path
+    click_on "All users"
+    expect(page).to have_content user.name
+  end
+
+  scenario "ログインしていないユーザーがLog inをクリックするとログイン画面へ" do
+    visit root_path
+    click_on "Log in"
+    expect(page).to have_content "Email"
+    expect(page).to have_content "Password"
+    expect(page).to have_content "Remember me"
+    expect(page).to have_button "Log in"
   end
 
   scenario "ログインしていないユーザーが投稿しようとするとログイン画面へ" do
